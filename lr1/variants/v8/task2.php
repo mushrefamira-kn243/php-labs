3fc1d60e8fd5a74059e8650b52e9de9384d9110e
Amira Mushref <kn243_maa1@student.ztu.edu.ua>
2026-02-16 18:40:41 +0200

    refactor: restructure task2.php for formatted text output; remove currency conversion logic

--- lr1/variants/v8/task2.php
+++ lr1/variants/v8/task2.php
@@ -1,11 +1,21 @@
 <?php
-$uah = 6350;
+/**
+ * Завдання 1: Форматований текст (варіант 8)
+ */
+require_once __DIR__ . '/layout.php';
 
-$usd_rate = 38.20;
+ob_start();
+?>
+<div class="poem">
+    <?php
+    echo "<p style='margin-left: 20px;'>Котик <b>спить</b> на теплій печі,</p>";
+    echo "<p style='margin-left: 20px;'>За вікном мете <i>холодна</i> хуртовина,</p>";
+    echo "<p style='margin-left: 20px;'>Бабуся вяже рукавиці,</p>";
+    echo "<p style='margin-left: 20px;'>А в хаті пахне калина.</p>";
+    ?>
+</div>
+<?php
+$content = ob_get_clean();
 
-$usd = $uah / $usd_rate;
-
-$usd_formatted = number_format($usd, 2);
-
-echo "{$uah} грн. можна обміняти на {$usd_formatted} долар";
+renderVariantLayout($content, 'Завдання 1', 'task2-body');
 ?>
