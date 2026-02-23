8a7bc696a890f526fddf518c3b41d23edc1c23d4
Amira Mushref <kn243_maa1@student.ztu.edu.ua>
2026-02-23 12:46:07 +0200

    refactor: update task 1 formatting; replace inline styles with CSS classes

--- lr1/variants/v8/task2.php
+++ lr1/variants/v8/task2.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * Завдання 1: Форматований текст (варіант 8)
+ * Завдання 1: Форматований текст
+ *
+ * Вірш про котика з форматуванням: <b>, <i>, margin-left
  */
 require_once __DIR__ . '/layout.php';
 
@@ -8,14 +10,13 @@ ob_start();
 ?>
 <div class="poem">
     <?php
-    echo "<p style='margin-left: 20px;'>Котик <b>спить</b> на теплій печі,</p>";
-    echo "<p style='margin-left: 20px;'>За вікном мете <i>холодна</i> хуртовина,</p>";
-    echo "<p style='margin-left: 20px;'>Бабуся вяже рукавиці,</p>";
-    echo "<p style='margin-left: 20px;'>А в хаті пахне калина.</p>";
+    echo "<p class='poem-indent-1'>Котик <b>спить</b> на теплій печі,</p>";
+    echo "<p class='poem-indent-1'>За вікном мете <i>холодна</i> хуртовина,</p>";
+    echo "<p class='poem-indent-1'>Бабуся вяже рукавиці,</p>";
+    echo "<p class='poem-indent-1'>А в хаті пахне калина.</p>";
     ?>
 </div>
 <?php
 $content = ob_get_clean();
 
 renderVariantLayout($content, 'Завдання 1', 'task2-body');
-?>
