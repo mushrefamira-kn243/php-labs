02a5f6a150abc1b7a99b84add6add4cfd9ef2d5f
Amira Mushref <kn243_maa1@student.ztu.edu.ua>
2026-02-23 11:51:55 +0200

    refactor: remove HTML structure and color table generation; redirect to task7_table.php

--- lr1/variants/v8/task6_table.php
+++ lr1/variants/v8/task6_table.php
@@ -1,35 +1,4 @@
-<!DOCTYPE html>
-<html lang="uk">
-<head>
-    <meta charset="UTF-8">
-    <title>Завдання 6.1</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            margin-bottom: 30px;
-        }
-        td {
-            width: 50px;
-            height: 50px;
-        }
-    </style>
-</head>
-<body>
 <?php
-function generateColorTable($rows, $cols) {
-    echo "<table border='1'>";
-    for ($i = 0; $i < $rows; $i++) {
-        echo "<tr>";
-        for ($j = 0; $j < $cols; $j++) {
-            $color = sprintf('#%06X', mt_rand(0, 0xFFFFFF));
-            echo "<td style='background-color: $color;'></td>";
-        }
-        echo "</tr>";
-    }
-    echo "</table>";
-}
-
-generateColorTable(4, 8);
-?>
-</body>
-</html>
+// Redirect to the template-aware page
+header('Location: task7_table.php');
+exit;
